cae2ff10042a0fa14532db25699705fa71deca99
luandoan <luandoan263@gmail.com>
2016-07-05 15:04:17 -0400

    new mbzirc + gazebo

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,25 @@
+# CMake generated Testfile for 
+# Source directory: /home/luan/mbzirc/src
+# Build directory: /home/luan/mbzirc/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(test/mybot_control)
+SUBDIRS(test/mybot_description)
+SUBDIRS(husky/husky_msgs)
+SUBDIRS(universal_robot/universal_robot)
+SUBDIRS(universal_robot/ur_bringup)
+SUBDIRS(universal_robot/ur_description)
+SUBDIRS(universal_robot/ur_gazebo)
+SUBDIRS(universal_robot/ur_msgs)
+SUBDIRS(husky/husky_control)
+SUBDIRS(husky/husky_description)
+SUBDIRS(husky/husky_navigation)
+SUBDIRS(husky/husky_ur5_moveit_config)
+SUBDIRS(mbzirc_gazebo)
+SUBDIRS(test/mybot_gazebo)
+SUBDIRS(universal_robot/ur_driver)
+SUBDIRS(moveit_core)
+SUBDIRS(universal_robot/ur5_moveit_config)
+SUBDIRS(universal_robot/ur_kinematics)
